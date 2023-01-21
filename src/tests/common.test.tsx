a56d6760527bee6a19bd1a3208d3904ae2ba5ce7
youneszahzouh-itexc <youneszahzouhitexc@gmail.com>
2023-01-21 20:01:10 +0100

    setup changes

--- src/tests/common.test.tsx
+++ src/tests/common.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 
 import 'jest-canvas-mock'
 
-import { WaveformAudioRecorder } from '../src/components'
+import { WaveformAudioRecorder } from '../components'
 
 describe('Common render', () => {
   it('renders without crashing', () => {
